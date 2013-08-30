061eb7308ef6aae6b8aa52c2bf8cc12ff9552987
Alexis Murzeau <amurzeau@gmail.com>
2013-08-30 16:22:19 +0200

    Add rank error count based on hop-by-hop option in ipv6 header

--- data_info/node.h
+++ data_info/node.h
@@ -56,6 +56,8 @@ void node_set_dtsn(di_node_t *node, int dtsn);
 void node_set_dao_seq(di_node_t *node, int dao_seq);
 void node_update_dao_interval(di_node_t *node, double timestamp);
 void node_update_dio_interval(di_node_t *node, double timestamp);
+void node_add_upward_error(di_node_t *node);
+void node_add_downward_error(di_node_t *node);
 
 const di_node_key_t *node_get_key(const di_node_t *node);
 addr_wpan_t node_get_mac64(const di_node_t *node);
@@ -74,6 +76,8 @@ int node_get_dtsn(const di_node_t *node);
 int node_get_dao_seq(const di_node_t *node);
 double node_get_max_dao_interval(const di_node_t *node);
 double node_get_max_dio_interval(const di_node_t *node);
+int node_get_upward_error_count(const di_node_t *node);
+int node_get_downward_error_count(const di_node_t *node);
 
 
 #ifdef	__cplusplus
