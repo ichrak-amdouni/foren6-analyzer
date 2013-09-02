21d211a3ea47a70aa8bcd3fb4a23724c52f1dc6f
Alexis Murzeau <amurzeau@gmail.com>
2013-09-02 15:41:07 +0200

    Filter out non ipv6 packet by tshark, it can crash on fragmented packets on some version. This does not alter frame number.

--- sniffer_packet_parser.c
+++ sniffer_packet_parser.c
@@ -222,7 +222,7 @@ static void sniffer_parser_reset() {
 #ifdef USE_NEW_TSHARK
 	if(spawn_piped_process("tshark", (char* const[]){"tshark", "-i", "-", "-V", "-T", "pdml", "-2", "-R", "ipv6", "-l", NULL}, &tshark_pid, &pipe_tshark_stdin, &pipe_tshark_stdout) == false) {
 #else
-	if(spawn_piped_process("tshark", (char* const[]){"tshark", "-i", "-", "-V", "-T", "pdml", "-l", NULL}, &tshark_pid, &pipe_tshark_stdin, &pipe_tshark_stdout) == false) {
+	if(spawn_piped_process("tshark", (char* const[]){"tshark", "-i", "-", "-V", "-T", "pdml", "-R", "ipv6", "-l", NULL}, &tshark_pid, &pipe_tshark_stdin, &pipe_tshark_stdout) == false) {
 #endif
 		perror("Can't spawn tshark process");
 		return;
