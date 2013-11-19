0da1c5991d405531d454886313cac6f598777268
Laurent Deru <laurent.deru@cetic.be>
2013-11-19 11:44:18 +0100

    Forget glocal autoconf address when updating dodag

--- src/data_info/node.c
+++ src/data_info/node.c
@@ -290,6 +290,9 @@ node_set_dodag(di_node_t * node, const di_dodag_ref_t * dodag_ref)
         node->rpl_instance_data.has_metric = false;
         node->has_rpl_dodag_config = false;
         node->has_rpl_dodag_prefix_info = false;
+        if ( !node->sixlowpan_config.is_custom_global_address ) {
+            init_ipv6_addr(&node->sixlowpan_config.global_address);
+        }
         node_set_changed(node);
     }
 }
