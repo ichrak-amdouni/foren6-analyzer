6c20cf3bf50a042a241ac9a33579c729347d5571
Laurent Deru <laurent.deru@cetic.be>
2014-11-26 08:58:29 +0100

    Add missing baudrate defines for MacOS-X

--- src/interface_reader/interfaces_mgr.c
+++ src/interface_reader/interfaces_mgr.c
@@ -40,6 +40,45 @@
 #include "../sha1.h"
 #include "../sniffer_packet_parser.h"
 
+#if __APPLE__
+#ifndef B460800
+#define B460800 460800
+#endif
+#ifndef B500000
+#define B500000 500000
+#endif
+#ifndef B576000
+#define B576000 576000
+#endif
+#ifndef B921600
+#define B921600 921600
+#endif
+#ifndef B1000000
+#define B1000000 1000000
+#endif
+#ifndef B1152000
+#define B1152000 1152000
+#endif
+#ifndef B1500000
+#define B1500000 1500000
+#endif
+#ifndef B2000000
+#define B2000000 2000000
+#endif
+#ifndef B2500000
+#define B2500000 2500000
+#endif
+#ifndef B3000000
+#define B3000000 3000000
+#endif
+#ifndef B3500000
+#define B3500000 3500000
+#endif
+#ifndef B4000000
+#define B4000000 4000000
+#endif
+#endif
+
 #define LAST_PACKET_NUMBER 100
 #define MINIMAL_CAPTURE_VERSION 0
 #define RECOMMENDED_CAPTURE_VERSION 1
